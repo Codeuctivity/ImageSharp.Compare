d00c7c9984f755618dae1dc721df9c3dd21a59ea
Stefan Seeland <stefan.seeland@gmail.com>
2022-05-15 16:28:28 +0200

    Fixed minor linter warnngn in updateNuget.sh

--- .vscode/updateNuget.sh
+++ .vscode/updateNuget.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 regex='PackageReference Include="([^"]*)" Version="([^"]*)"'
-find . -name "*.*proj" | while read proj; do
-    while read line; do
+find . -name "*.*proj" | while read -r proj; do
+    while read -r line; do
         if [[ $line =~ $regex ]]; then
             name="${BASH_REMATCH[1]}"
             version="${BASH_REMATCH[2]}"
             if [[ $version != *-* ]]; then
-                dotnet add $proj package $name
+                dotnet add "$proj" package "$name"
             fi
         fi
-    done <$proj
+    done <"$proj"
 done
