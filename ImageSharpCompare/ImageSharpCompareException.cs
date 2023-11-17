db688912b55c12a9f420f16e144fe23b3013323a
Stefan Seeland <168659+stesee@users.noreply.github.com>
2023-11-17 21:33:13 +0100

    Dropped .net6 target

--- ImageSharpCompare/ImageSharpCompareException.cs
+++ ImageSharpCompare/ImageSharpCompareException.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Runtime.Serialization;
 
 namespace Codeuctivity.ImageSharpCompare
 {
     /// <summary>
     /// ImageSharpCompareException gets thrown if comparing of images fails
     /// </summary>
-    [Serializable]
     public class ImageSharpCompareException : Exception
     {
         /// <summary>
@@ -32,16 +30,6 @@ namespace Codeuctivity.ImageSharpCompare
         /// <param name="innerException"></param>
         /// <returns></returns>
         public ImageSharpCompareException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        /// <summary>
-        /// ImageSharpCompareException gets thrown if comparing of images fails
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        protected ImageSharpCompareException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
